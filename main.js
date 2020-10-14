b9fe146c59bc373547a850e6ad16ddd27f31e623
Maria Francis-Moullier <moullier@gmail.com>
2020-10-13 23:37:06 -0700

    generates one piece and can move it back and forth, no collision detection yet

--- main.js
+++ main.js
@@ -13,7 +13,36 @@ ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
 let board = new Board();
 
-function play() {  
-  board.reset();  
-  console.table(board.grid);  
+function play() {
+  board.reset();
+  let piece = new Piece(ctx);
+  piece.draw();
+  
+  board.piece = piece;
 }
+
+const moves = {
+  [KEY.LEFT]:  p => ({ ...p, x: p.x - 1 }),
+  [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
+  [KEY.DOWN]:    p => ({ ...p, y: p.y + 1 })
+};
+
+document.addEventListener('keydown', event => {
+  if (moves[event.keyCode]) {  
+    // Stop the event from bubbling.
+    event.preventDefault();
+    
+    // Get new state of piece
+    let p = moves[event.keyCode](board.piece);
+    
+    // if (board.valid(p)) {    
+      // If the move is valid, move the piece.
+      board.piece.move(p);
+      
+      // Clear old position before drawing.
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); 
+      
+      board.piece.draw();
+    // }
+  }
+});
